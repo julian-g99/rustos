c2d479a4add512546a5c8b61b7e815616c36dd07
Julian Gu <j.gush99@gmail.com>
2020-02-07 17:00:29 -0500

    changes to gpio

--- lib/pi/src/gpio.rs
+++ lib/pi/src/gpio.rs
@@ -103,7 +103,10 @@ impl Gpio<Uninitialized> {
     /// Enables the alternative function `function` for `self`. Consumes self
     /// and returns a `Gpio` structure in the `Alt` state.
     pub fn into_alt(self, function: Function) -> Gpio<Alt> {
-        unimplemented!()
+        let n = self.pin as usize / 10;
+        let one_digit = self.pin % 10;
+        self.registers.FSEL[n].write((function as u32) << (one_digit * 3));
+        self.transition()
     }
 
     /// Sets this pin to be an _output_ pin. Consumes self and returns a `Gpio`
--- lib/pi/src/timer.rs
+++ lib/pi/src/timer.rs
@@ -32,17 +32,24 @@ impl Timer {
     /// Reads the system timer's counter and returns Duration.
     /// `CLO` and `CHI` together can represent the number of elapsed microseconds.
     pub fn read(&self) -> Duration {
-        unimplemented!()
+        let system_time = (self.registers.CHI.read() as u64) << 32 + self.registers.CLO.read() as u64;
+        Duration::from_micros(system_time)
     }
 }
 
 /// Returns current time.
 pub fn current_time() -> Duration {
-    unimplemented!()
+    let timer = Timer::new();
+    timer.read()
 }
 
 /// Spins until `t` duration have passed.
 pub fn spin_sleep(t: Duration) {
-    unimplemented!()
+    let end_time = current_time() + t;
+    loop {
+        if current_time() == end_time {
+            break;
+        }
+    }
 }
 
