7eeb8358a3b8f5a6bee1c92c5e0647269f4b789f
Julian Gu <j.gush99@gmail.com>
2020-02-09 03:15:07 -0500

    console works

--- kern/src/console.rs
+++ kern/src/console.rs
@@ -60,7 +60,7 @@ impl io::Write for Console {
 
 impl fmt::Write for Console {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        unimplemented!()
+        self.inner().write_str(s)
     }
 }
 
