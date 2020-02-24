b6e524d7314428097cf5cb6ec4264a32948770c7
Julian Gu <j.gush99@gmail.com>
2020-02-24 14:37:26 -0500

    finished up to cached partition

--- lib/fat32/src/mbr.rs
+++ lib/fat32/src/mbr.rs
@@ -4,8 +4,8 @@ use shim::io;
 
 use crate::traits::BlockDevice;
 
-#[repr(C)]
-#[derive(Copy, Clone)]
+#[repr(C, packed)]
+#[derive(Copy, Clone, Default)]
 pub struct CHS {
     head: u8,
     sector_cylinder: u16
@@ -14,13 +14,14 @@ pub struct CHS {
 // FIXME: implement Debug for CHS
 impl fmt::Debug for CHS {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "head: {:?}, sector and cylinder: {:?}", self.head, self.sector_cylinder)
+        write!(f, "head: {:?}, sector and cylinder: {:?}", self.head, &{self.sector_cylinder})
     }
 }
 
 //const_assert_size!(CHS, 3);
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 pub struct PartitionEntry {
     boot_indicator: u8,
     head_chs: CHS,
@@ -45,11 +46,12 @@ impl fmt::Debug for PartitionEntry {
 
 /// The master boot record (MBR).
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 pub struct MasterBootRecord {
     bootstrap: [u8; 436],
     disk_id: [u8; 10],
     partition_entries: [PartitionEntry; 4],
-    valid_siganture: u16
+    valid_signature: u16
 }
 
 // FIXME: implemente Debug for MaterBootRecord
@@ -57,7 +59,7 @@ impl fmt::Debug for MasterBootRecord {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "unique disk id: {:?}, partition entries: {:?}, \
             valid sector signature bytes: {:?}",
-             self.disk_id, self.partition_entries, &{self.valid_siganture})
+            self.disk_id, self.partition_entries, &{self.valid_signature})
     }
 }
 
@@ -83,6 +85,28 @@ impl MasterBootRecord {
     /// boot indicator. Returns `Io(err)` if the I/O error `err` occured while
     /// reading the MBR.
     pub fn from<T: BlockDevice>(mut device: T) -> Result<MasterBootRecord, Error> {
-        unimplemented!("MasterBootRecord::from()")
+        //let sector_size = device.sector_size();
+        //let buf: &mut[u8] = &mut ([0u8; 512]); //CHECK: is it ok to just use 512 here?
+        let mut arr = [0u8; 512];
+        let buf = &mut arr[..];
+        match device.read_sector(0, buf) {
+            Err(e) => {
+                return Err(Error::Io(e));
+            },
+            Ok(_) => {
+                let mbr = unsafe{ *(buf.as_mut_ptr() as *mut MasterBootRecord) };
+                //let mbr = unsafe {core::mem::transmute::<}
+                if mbr.valid_signature != 0xAA55 {
+                    return Err(Error::BadSignature);
+                }
+                for i in 0..4 {
+                    let partition = mbr.partition_entries[i];
+                    if partition.boot_indicator != 0 && partition.boot_indicator != 0x80 {
+                        return Err(Error::UnknownBootIndicator(i as u8));
+                    }
+                }
+                return Ok(mbr);
+            }
+        }
     }
 }
--- lib/fat32/src/vfat/ebpb.rs
+++ lib/fat32/src/vfat/ebpb.rs
@@ -5,8 +5,37 @@ use crate::traits::BlockDevice;
 use crate::vfat::Error;
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 pub struct BiosParameterBlock {
-    // FIXME: Fill me in.
+    first_three: [u8; 3],
+    oem_id: [u8; 8],
+    bytes_per_sector: u16,
+    sectors_per_cluster: u8,
+    num_reserved_sectors: u16,
+    num_fats: u8,
+    num_dir_entries: u16,
+    num_logical_sectors: u16,
+    fat_id: u8,
+    num_sectors_per_fat: u16, //NOTE: should be 0 for FAT 32
+    num_sectors_per_track: u16,
+    num_heads: u16,
+    num_hidden_sectors: u32,
+    total_logical_sectors: u32,
+    sectors_per_fat: u32,
+    flags: u16,
+    fat_version_number: u16,
+    num_clusters_in_root_dir: u32,
+    fs_info_sector: u16,
+    backup_boot_sector: u16,
+    reserved: [u8; 12],
+    drive_number: u8,
+    flag_windows: u8,
+    signature: u8,
+    volume_id: u32,
+    volumne_label_string: [u8; 11],
+    system_identifier_string: [u8; 8],
+    boot_code:[u8; 420],
+    boot_signature: u16
 }
 
 //const_assert_size!(BiosParameterBlock, 512);
@@ -19,12 +48,30 @@ impl BiosParameterBlock {
     ///
     /// If the EBPB signature is invalid, returns an error of `BadSignature`.
     pub fn from<T: BlockDevice>(mut device: T, sector: u64) -> Result<BiosParameterBlock, Error> {
-        unimplemented!("BiosParameterBlock::from()")
+        let mut arr = [0u8; 512];
+        let buf = &mut arr[..];
+        match device.read_sector(sector, buf) {
+            Err(e) => {
+                return Err(Error::Io(e));
+            },
+            Ok(_) => {
+                let ebpb = unsafe{ *(buf.as_mut_ptr() as *mut BiosParameterBlock) };
+                if ebpb.boot_signature != 0xAA55 {
+                    dbg!(ebpb.signature);
+                    return Err(Error::BadSignature);
+                }
+                return Ok(ebpb);
+            }
+        }
     }
 }
 
 impl fmt::Debug for BiosParameterBlock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        unimplemented!("BiosParameterBlock::fmt()")
+        //unimplemented!("BiosParameterBlock::fmt()")
+        write!(f, "num bytes per sector: {:?}, num sectors per cluster: {:?}, \
+            num fats:{:?}, total logical directories: {:?}, sectors per fat: {:?}, signature: {:?}, \
+            boot parition signature: {:?}", &{self.bytes_per_sector}, self.sectors_per_cluster,
+            self.num_fats, &{self.total_logical_sectors}, &{self.sectors_per_fat}, self.signature, &{self.boot_signature})
     }
 }
